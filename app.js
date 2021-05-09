b8411a9cfe8926c79734321d043167d0d6db94ec
PlehanovEvgeniy <72038550+PlehanovEvgeniy@users.noreply.github.com>
2021-05-09 22:17:44 +0300

    add api architecture

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const users = require('./routes/users');
+const cards = require('./routes/cards');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6098307b01024131f8b3cd2d',
+  };
+
+  next();
+});
+
+app.use('/users', users);
+app.use('/cards', cards);
+app.use((req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
+
+app.listen(PORT, () => {
+  console.log(`server started, Port: ${PORT}`);
+});
